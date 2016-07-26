0796b8cd3cdcfd2aaa88e7fd51cdb018cd1c2d95
knolegsb <seon2you@hotmail.com>
2016-07-26 00:42:12 -0700

    added views

--- Mvc5CMS/Models/Post.cs
+++ Mvc5CMS/Models/Post.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mvc5CMS.Models
 {
     public class Post
     {
+        [Display(Name = "Slug")]
         public string Id { get; set; }
+
+        [Display(Name = "Title")]
         public string Title { get; set; }
+
+        [Display(Name = "Post Content")]
         public string Content { get; set; }
+
+        [Display(Name = "Date Created")]
         public DateTime Created { get; set; }
+
+        [Display(Name = "Date Published")]
         public DateTime? Published { get; set; }
+
         public int AuthorId { get; set; }
     }
 }
