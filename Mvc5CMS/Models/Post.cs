c05288c1679cdc8ec4e6ddf657c0c6c9783ff642
knolegsb <seon2you@hotmail.com>
2016-07-25 23:28:55 -0700

    added admin area and post controller

--- Mvc5CMS/Models/Post.cs
+++ Mvc5CMS/Models/Post.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Mvc5CMS.Models
 {
     public class Post
     {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Published { get; set; }
+        public int AuthorId { get; set; }
     }
 }
