3c87c0305f9744c249c8a575958491af29003c18
knolegsb <seon2you@hotmail.com>
2016-07-26 02:57:13 -0700

    conducted unit testing

---
+++ Mvc5CMS/Data/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Mvc5CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc5CMS.Data
+{
+    public interface IPostRepository
+    {
+        Post Get(string id);
+        void Edit(string id, Post updatedItem);
+        void Create(Post model);
+        IEnumerable<Post> GetAll();
+    }
+}
