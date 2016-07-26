071cba0740ce85c78150beb7cfb67fb2be2c3ed1
knolegsb <seon2you@hotmail.com>
2016-07-26 02:57:41 -0700

    added untracked files

---
+++ Mvc5CMS.Tests/Admin/Controllers/PostControllerTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mvc5CMS.Areas.Admin.Controllers;
+using Mvc5CMS.Data;
+using Mvc5CMS.Models;
+using System.Web.Mvc;
+using Telerik.JustMock;
+
+namespace Mvc5CMS.Tests.Admin.Controllers
+{
+    [TestClass]
+    public class PostControllerTest
+    {
+        [TestMethod]
+        public void Edit_GetRequestSendsPostToView()
+        {
+            var id = "test-post";
+
+            var repo = Mock.Create<IPostRepository>();
+            var controller = new PostController(repo);
+
+            Mock.Arrange(() => repo.Get(id)).
+                Returns(new Post { Id = id });
+
+            var result = (ViewResult)controller.Edit(id);
+            var model = (Post)result.Model;
+
+            Assert.AreEqual(id, model.Id);
+        }
+
+        [TestMethod]
+        public void Edit_GetRequestNotFoundResult()
+        {
+            var id = "test-post";
+
+            var repo = Mock.Create<IPostRepository>();
+            var controller = new PostController(repo);
+
+            Mock.Arrange(() => repo.Get(id)).
+                Returns((Post)null);
+
+            var result = controller.Edit(id);
+            //var model = (Post)result.Model;
+
+            Assert.IsTrue(result is HttpNotFoundResult);
+        }
+
+        [TestMethod]
+        public void Edit_PostRequestNotFoundResult()
+        {
+            var id = "test-post";
+
+            var repo = Mock.Create<IPostRepository>();
+            var controller = new PostController(repo);
+
+            Mock.Arrange(() => repo.Get(id)).
+                Returns((Post)null);
+
+            var result = controller.Edit(id, new Post());
+            //var model = (Post)result.Model;
+
+            Assert.IsTrue(result is HttpNotFoundResult);
+        }
+
+        [TestMethod]
+        public void Edit_PostRequestSendsPostToView()
+        {
+            var id = "test-post";
+
+            var repo = Mock.Create<IPostRepository>();
+            var controller = new PostController(repo);
+
+            Mock.Arrange(() => repo.Get(id)).
+                Returns(new Post() {Id = id});
+
+            controller.ViewData.ModelState.AddModelError("key", "error message");
+
+            var result = (ViewResult)controller.Edit(id, new Post() {Id = "test-post-2"});
+            var model = (Post)result.Model;
+
+            Assert.AreEqual("test-post-2", model.Id);
+        }
+
+        [TestMethod]
+        public void Edit_PostRequestCallsEditAndRedirects()
+        {
+            var repo = Mock.Create<IPostRepository>();
+            var controller = new PostController(repo);
+
+            Mock.Arrange(() => repo.Edit(Arg.IsAny<string>(), Arg.IsAny<Post>())).MustBeCalled();
+                
+            //controller.ViewData.ModelState.AddModelError("key", "error message");
+
+            var result = controller.Edit("foo", new Post() {Id = "test-post-2"});
+            
+            Mock.Assert(repo);
+
+            Assert.IsTrue(result is RedirectToRouteResult);
+        }
+    }
+}
---
+++ Mvc5CMS/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,89 @@
+﻿using Mvc5CMS.Data;
+using Mvc5CMS.Models;
+using System.Web.Mvc;
+
+namespace Mvc5CMS.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [RoutePrefix("post")]
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _repository;
+
+        public PostController(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Admin/Post/Create
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            var model = new Post();
+            return View(model);
+        }
+
+        // Admin/Post/Create
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // TODO: update model in data store
+
+            return RedirectToAction("Index");
+        }
+
+        // Admin/Post/Edit/Post-To-Edit
+        [HttpGet]
+        [Route("edit/{postId}")]
+        public ActionResult Edit(string postId)
+        {
+            // TODO: to retrieve the model from the data store
+            var post = _repository.Get(postId);
+            //var model = new Post();
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(post);
+        }
+
+        // Admin/Post/Edit/Post-To-Edit
+        [HttpPost]
+        [Route("edit/{postId}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string postId, Post model)
+        {
+            var post = _repository.Get(postId);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _repository.Edit(postId, model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
